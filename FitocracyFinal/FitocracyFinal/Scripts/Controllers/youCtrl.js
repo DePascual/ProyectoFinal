67fcb34b0125eaeaf7c5c728c7d8fdd744b99d05
DePascual <carolinadepascualjimenez@gmail.com>
2016-05-23 20:32:15 +0200

    compilando div de UserInfo

--- FitocracyFinal/FitocracyFinal/Scripts/Controllers/youCtrl.js
+++ FitocracyFinal/FitocracyFinal/Scripts/Controllers/youCtrl.js
@@ -1,12 +1,16 @@
 ﻿angular.module('Fitocracy')
-    .controller('youCtrl', function ($scope) {
+    .controller('youCtrl', function ($scope, $window, $location, $compile) {
+
 
         $scope.cargaPartial = function (obj) {
             var idLink = obj.target.attributes.id.value;
 
             switch (idLink) {
                 case "userInfo":
-                    $('#vistaParcial').load("http://localhost:2841/PartialsViews/UserInfo");
+                    $('#vistaParcial').load("http://localhost:2841/PartialsViews/UserInfo", function () {
+                        alert('deberia de estar cargada');
+                        $compile($('#vistaParcial'))($scope);
+                    });
                     break;
                 case "userWorkouts":
                     $('#vistaParcial').load("http://localhost:2841/PartialsViews/UserWorkouts");
@@ -18,6 +22,19 @@
                     $('#vistaParcial').load("http://localhost:2841/PartialsViews/SummaryLevels");
                     break;
             }
-        }
+
+           
+        };
+
+        $scope.actualizar = function (isValid) {
+            if (isValid) {
+                var usuario = {
+                    Username: $scope.uName,
+                }
+            }
+        };
+
+        $scope.mensa = "hollllaaaa"
+
 
     })
